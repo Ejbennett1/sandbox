f1696d3f12f93d13bb14e5f6bdcdb6711d2354d9
Ethan <ethanjbennett@students.abtech.edu>
2026-09-02 13:04:54 -0400

    Initial commit

---
+++ sandbox/oop_intro.php
@@ -0,0 +1,36 @@
+<?php
+
+class instrument {
+    // class properties
+    public string $name;
+    public string $tuning;
+    public string $cost;
+
+    // class methods
+    public function play(): void
+    {
+        echo("The {$this->name} is playing. <br>");
+    }
+
+    public function showTuning (): void
+    {
+        echo("The {$this->name} is tuned to {$this->tuning}.<br>");
+    }
+}
+
+$fiddle = new instrument();
+$fiddle->name = "Fiddle";
+$fiddle->tuning = "G-D-A-E";
+
+// Create another instance of the instrument class
+$guitar = new instrument();
+$guitar ->name = "Lowden Guitar";
+$guitar->tuning = "D-A-D-G-A-D";
+
+//echo "The {$fiddle->name} is tuned to {$fiddle->tuning}.<br>";
+//echo "The {$guitar->name} is tuned to {$guitar->tuning}.<br>";
+
+$fiddle->play();
+$fiddle->showTuning();
+$guitar->play();
+$guitar->showTuning();
