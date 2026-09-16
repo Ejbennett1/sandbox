c19c6d9d09c2ba18324245c84c438e90c92f2876
Ethan <ethanjbennett@students.abtech.edu>
2026-09-16 00:56:02 -0400

    Start asgn03 static assignment

---
+++ asgn03-static/Bird.php
@@ -0,0 +1,41 @@
+<?php
+
+class Bird {
+    var $habitat;
+    var $food;
+    var $nesting = "tree";
+    var $conservation;
+    var $song = "chirp";
+   
+    public static $flying = "yes";
+
+    public static $instance_count = 0;
+    public static $egg_num = 0;
+
+    public static function create() {
+        static::$instance_count++;
+        return new static();
+    }
+
+    function can_fly() {
+        $flying_string = (static::$flying == "yes") ?"can fly" :"is stuck on the ground";
+        return $flying_string;
+    }
+}
+
+class YellowBelliedFlyCatcher extends Bird {
+    var $name = "yellow-bellied flycatcher";
+    var $diet = "mostly insects.";
+    var $song = "flat chilk";
+    
+    public static $egg_num = "3-4, sometimes 5.";
+}
+
+
+
+class Kiwi extends Bird {
+    var $name = "kiwi";
+    var $diet = "omnivorous";
+
+    public static $flying = "no";
+}
